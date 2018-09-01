4973a30333b2ac5577993e7758018948f294eb74
alejox300 <36076379+alejox300@users.noreply.github.com>
2018-09-01 11:09:50 -0500

    Video 6

---
+++ Sales/Sales/Services/ApiService.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Sales.Services
+{
+    using System;
+    using System.Threading.Tasks;
+    using Sales.
+
+    public class ApiService
+    {
+        public async Task<Response> GetList<T>(string urlBase, string prefix, string controller)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                }
+
+                throw;
+            }
+        }
+    }
+}
