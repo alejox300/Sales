5c393df94cded6ecf3c95b005e64b1192d7de3f5
alejox300 <36076379+alejox300@users.noreply.github.com>
2018-09-03 20:59:52 -0500

    video 10

--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -8,10 +8,14 @@ namespace Sales.ViewModels
     using System.Text;
     using Services;
     using Xamarin.Forms;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
 
     public class ProductsViewModel : BaseViewModel
     {
         private ApiService apiService;
+
+        private bool isRefreshing;
 
         private ObservableCollection<Product> products; 
 
@@ -19,6 +23,12 @@ namespace Sales.ViewModels
         {
             get { return this.products; }
             set { this.SetValue(ref this.products, value); }
+        }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
         }
 
         public ProductsViewModel()
@@ -29,16 +39,37 @@ namespace Sales.ViewModels
 
         private async void LoadProducts()
         {
-            var response = await this.apiService.GetList<Product>("https://salesapiservices.azurewebsites.net", "/api", "/Products");
+            this.IsRefreshing = true;
+
+            var connection = await this.apiService.CheckConnection();
+
+            if (!connection.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert("Error", connection.Message, "Accept");
+                return;
+
+            }
+
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var response = await this.apiService.GetList<Product>(url, "/api", "/Products");
             if (!response.IsSuccess)
             {
+                this.IsRefreshing =false;
                 await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
                 return;
             }
 
             var list = (List<Product>)response.Result;
             this.Products = new ObservableCollection<Product>(list);
-
+            this.IsRefreshing = false;
+        }
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadProducts);
+             }
         }
     }
 }
